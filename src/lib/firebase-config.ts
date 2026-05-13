578f241b50078e6997fe0c0040de2861d53985d4
Jae Woo <jaewoo@MacBook-Pro.local>
2026-05-13 12:45:44 +0545

    app update 7

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  projectId: "sound-shepherd",
+  appId: "1:625085868246:web:95103444588909f0810177",
+  apiKey: "AIzaSyA-rXipPm92uTPiCP5IELjZKy7q5stsBeI",
+  authDomain: "sound-shepherd.firebaseapp.com",
+  firestoreDatabaseId: "ai-studio-soundshepherd-79ef04cf-30e0-43fe-9657-d7db30b9fbb7",
+  storageBucket: "sound-shepherd.firebasestorage.app",
+  messagingSenderId: "625085868246",
+  measurementId: ""
+};
